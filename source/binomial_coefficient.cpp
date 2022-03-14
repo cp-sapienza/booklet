93aec063074bae6ce34a880c4bbece7f33fcba39
DannyAntonelli <dannyantonelli2002@gmail.com>
2022-03-14 18:45:32 +0100

    Add Catalan Number

--- source/binomial_coefficient.cpp
+++ source/binomial_coefficient.cpp
@@ -1,5 +1,5 @@
 int n_choose_k(int n, int k, int mod) {
 	assert(n >= k);
-	assert(factorial.size() >= n);
+	assert(factorial.size() > n);
 	return (((factorial[n] * inv_factorial[k]) % mod) * inv_factorial[n - k]) % mod;
 }
---
+++ source/catalan_number.cpp
@@ -0,0 +1,4 @@
+int catalan_number(int n, int mod) {
+    assert(factorial.size() > 2 * n);
+    return (((fact[2 * n] * invfact[n + 1]) % mod) * invfact[n]) % mod;
+}
