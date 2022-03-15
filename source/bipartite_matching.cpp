ba7ec88efa67ddb564cb92a9af7bc23075203cdf
Dario Petrillo <dario.pk1@gmail.com>
2022-03-15 15:32:56 +0100

    naming convention pt2

--- source/bipartite_matching.cpp
+++ source/bipartite_matching.cpp
@@ -1,8 +1,8 @@
 vector<pair<int,int>> bipartite_matching(vector<pair<int,int>>& edges, int n, int m) {
 	PushRelabel pr(n+m+2);
-	for(auto e : edges) pr.addEdge(e.first, n+e.second, 1);
-	for(int i = 0; i < n; i++) pr.addEdge(n+m, i, 1);
-	for(int i = 0; i < m; i++) pr.addEdge(n+i, n+m+1, 1);
+	for(auto e : edges) pr.add_edge(e.first, n+e.second, 1);
+	for(int i = 0; i < n; i++) pr.add_edge(n+m, i, 1);
+	for(int i = 0; i < m; i++) pr.add_edge(n+i, n+m+1, 1);
 	pr.calc(n+m, n+m+1);
 
 	vector<pair<int,int>> res;
