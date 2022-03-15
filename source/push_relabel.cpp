b1b076281c2eda9efe988c78a19702489ded8e6f
Dario Petrillo <dario.pk1@gmail.com>
2022-03-15 15:32:01 +0100

    naming convention

--- source/push_relabel.cpp
+++ source/push_relabel.cpp
@@ -9,13 +9,13 @@ struct PushRelabel {
 	vector<vi> hs; vi H;
 	PushRelabel(int n) : g(n), ec(n), cur(n), hs(2*n), H(n) {}
 
-	void addEdge(int s, int t, ll cap, ll rcap=0) {
+	void add_edge(int s, int t, ll cap, ll rcap=0) {
 		if (s == t) return;
 		g[s].push_back({t, ssize(g[t]), 0, cap});
 		g[t].push_back({s, ssize(g[s])-1, 0, rcap});
 	}
 
-	void addFlow(Edge& e, ll f) {
+	void add_flow(Edge& e, ll f) {
 		Edge &back = g[e.dest][e.back];
 		if (!ec[e.dest] && f) hs[H[e.dest]].push_back(e.dest);
 		e.f += f; e.c -= f; ec[e.dest] += f;
@@ -25,7 +25,7 @@ struct PushRelabel {
 		int v = ssize(g); H[s] = v; ec[t] = 1;
 		vi co(2*v); co[0] = v-1;
 		for(int i = 0; i < v; i++) cur[i] = g[i].data();
-		for (Edge& e : g[s]) addFlow(e, e.c);
+		for (Edge& e : g[s]) add_flow(e, e.c);
 
 		for (int hi = 0;;) {
 			while (hs[hi].empty()) if (!hi--) return -ec[s];
@@ -41,9 +41,9 @@ struct PushRelabel {
 								--co[H[i]], H[i] = v + 1;
 					hi = H[u];
 				} else if (cur[u]->c && H[u] == H[cur[u]->dest]+1)
-					addFlow(*cur[u], min(ec[u], cur[u]->c));
+					add_flow(*cur[u], min(ec[u], cur[u]->c));
 				else ++cur[u];
 		}
 	}
-	bool leftOfMinCut(int a) { return H[a] >= ssize(g); }
+	bool left_of_min_cut(int a) { return H[a] >= ssize(g); }
 };
