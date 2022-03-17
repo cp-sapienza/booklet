d606ed6062b016ea39b34834f342ce438741ba33
Dario Petrillo <dario.pk1@gmail.com>
2022-03-17 11:27:51 +0100

    shorten make_tuple -> make_pair

--- source/egcd.cpp
+++ source/egcd.cpp
@@ -1,11 +1,11 @@
-array<ll, 3> egcd(ll a, ll b) {
+array<ll,3> egcd(ll a, ll b) {
 	ll x = 1, y = 0;
 	ll x1 = 0, y1 = 1, a1 = a, b1 = b;
-	while (b1) {
+	while(b1) {
 		ll q = a1 / b1;
-		tie(x, x1) = make_tuple(x1, x - q * x1);
-		tie(y, y1) = make_tuple(y1, y - q * y1);
-		tie(a1, b1) = make_tuple(b1, a1 - q * b1);
+		tie(x, x1) = make_pair(x1, x - q * x1);
+		tie(y, y1) = make_pair(y1, y - q * y1);
+		tie(a1, b1) = make_pair(b1, a1 - q * b1);
 	}
 	return {x, y, a1};
 }
