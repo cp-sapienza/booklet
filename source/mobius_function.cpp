ab3892b0893c1ae8ec88fd058f6550aadfa482aa
DannyAntonelli <dannyantonelli2002@gmail.com>
2022-04-01 12:33:17 +0200

    Add Möbius function code

---
+++ source/mobius_function.cpp
@@ -0,0 +1,15 @@
+vi mu_up_to_n(int n) {
+	vi mu(n), lpf(n);
+	for (ll p = 2; p <= n; ++p)
+		if (!lpf[p]) {
+			lpf[p] = p;
+			for (ll k = p * p; k <= n; k += p)
+				if (!lpf[k])
+					lpf[k] = p;
+		}
+	mu[1] = 1;
+	for (int i = 2; i <= n; ++i)
+		if (lpf[i / lpf[i]] == lpf[i]) mu[i] = 0;
+		else mu[i] = -1 * mu[i / lpf[i]];
+	return mu;
+}
