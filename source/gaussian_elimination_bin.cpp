8d011df44fc4911d5e8bd13002217236323beecf
DannyAntonelli <dannyantonelli2002@gmail.com>
2022-04-21 09:52:36 +0200

    Change spaces to tabs

--- source/gaussian_elimination_bin.cpp
+++ source/gaussian_elimination_bin.cpp
@@ -7,23 +7,23 @@ int solveLinear(vector<bs>& A, vi& b, bs& x, int m) {
 	for(int i = 0; i < n; i++) {
 		for (br=i; br<n; ++br) if (A[br].any()) break;
 		if (br == n) {
-            for(int j = i; j < n; j++)
-			    if(b[j]) return -1;
+			for(int j = i; j < n; j++)
+				if(b[j]) return -1;
 			break;
 		}
 		int bc = (int)A[br]._Find_next(i-1);
 		swap(A[i], A[br]);
 		swap(b[i], b[br]);
 		swap(col[i], col[bc]);
-        for(int j = 0; j < n; j++)
-            if (A[j][i] != A[j][bc]) {
-			    A[j].flip(i); A[j].flip(bc);
-		    }
-        for(int j = i+1; j < n; j++)
-		    if (A[j][i]) {
-                b[j] ^= b[i];
-                A[j] ^= A[i];
-		    }
+		for(int j = 0; j < n; j++)
+			if (A[j][i] != A[j][bc]) {
+				A[j].flip(i); A[j].flip(bc);
+			}
+		for(int j = i+1; j < n; j++)
+			if (A[j][i]) {
+				b[j] ^= b[i];
+				A[j] ^= A[i];
+			}
 		rank++;
 	}
 
@@ -31,8 +31,8 @@ int solveLinear(vector<bs>& A, vi& b, bs& x, int m) {
 	for (int i = rank; i--;) {
 		if (!b[i]) continue;
 		x[col[i]] = 1;
-        for(int j = 0; j < i; j++)
-		    b[j] ^= A[j][i];
+		for(int j = 0; j < i; j++)
+			b[j] ^= A[j][i];
 	}
 	return rank; // multiple solutions if rank < m, none if -1
 }
