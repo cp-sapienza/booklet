14f22596bfabae486023caed9efe119f71f77454
Dario Petrillo <dario.pk1@gmail.com>
2022-04-26 14:54:55 +0200

    Save a few lines in phi_up_to_n

--- source/phi.cpp
+++ source/phi.cpp
@@ -13,12 +13,8 @@ int phi(int n) {
 vi phi_up_to_n(int n) {
 	vi phi(n+1);
 	for(int i = 0; i <= n; i++) phi[i] = i;
-	for(int i = 2; i <= n; i++) {
-		if(phi[i] == i) {
-			for(int j = i; j <= n; j += i) {
-				phi[j] -= phi[j] / i;
-			}
-		}
-	}
+	for(int i = 2; i <= n; i++) if(phi[i] == i)
+		for(int j = i; j <= n; j += i)
+			phi[j] -= phi[j] / i;
 	return phi;
 }
