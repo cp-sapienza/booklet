998db0f7109b70111416f0f45690206037c9b98b
andrea <andreapudd@gmail.com>
2022-11-04 12:43:28 +0100

    saving space

--- source/line.h
+++ source/line.h
@@ -3,11 +3,11 @@
 #include "point.h"
 #include "constants_geom.h"
 
+#define TTT template<typename T>
 struct Line{
 	ld a, b, c; // b = 1 non-vertical lines, b = 0 vertical lines
 	Line(ld a_, ld b_, ld c_) : a(a_), b(b_), c(c_) {}
-	template<typename T>
-	Line(const Point<T>& p1, const Point<T>& p2){
+	TTT Line(const Point<T>& p1, const Point<T>& p2){
 		if(abs(p1.x - p2.x) < EPS) {
 			a = 1.0; b = 0.0; c = -p1.x;
 		}
@@ -17,8 +17,7 @@ struct Line{
 			c = -(ld) (a * p1.x) - p1.y;
 		}
 	}
-	template<typename T>
-	Line(Point<T>& p, ld m) {
+	TTT Line(Point<T>& p, ld m) {
 		a = m; b = 1.0; c = -((a * p.x) + (b * p.y)); }
 	bool check_parallel(Line l) {
 		return (abs(a-l.a) < EPS) && (abs(b-l.b) < EPS); }
@@ -33,18 +32,15 @@ struct Line{
 		else p.y = -(l.a * p.x + c);
 		return true;
 	}
-	template<typename T>
-	Line operator + (Point<T> p) { // translate line by vector 
+	TTT Line operator + (Point<T> p) { // translate line by vector 
 		return Line(a, b, c-p.y*b-a*p.x);
 	}
 	friend ostream& operator << (ostream& os, Line l) {
 		return os << "(" << l.a << ", " << l.b  << ", " << l.c << ")"; }
 };
-template<typename T>
-ld dist_to_line(Point<T> p, Line l) {
+TTT ld dist_to_line(Point<T> p, Line l) {
 	return abs(l.a * p.x + l.b * p.y + l.c) / sqrtl(l.a * l.a + l.b * l.b); }
-template<typename T>
-Point<ld> closest_point(Point<T> p, Line ln) { // returns the closest point to p on l
+TTT Point<ld> closest_point(Point<T> p, Line ln) { // returns the closest point to p on l
 	ln = ln+(-p);
 	ld t = ln.c/(ln.a*ln.a+ln.b*ln.b);
 	return p+Point(-ln.a*t, -ln.b*t);
@@ -58,12 +54,7 @@ Point<ld> intersec_line_seg(Point<ld> p, Point<ld> q, Point<ld> A, Point<ld> B) 
 }
 // use this for line equations where b is not always equal to 0 or 1
 Line norm(ld a, ld b, ld c) {
-	if(abs(b) < EPS)
-		b = 0.0;
-	else {
-		a /= b;
-		c /= b;
-		b = 1.0;
-	}
+	if(abs(b) < EPS) b = 0.0;
+	else { a /= b; c /= b; b = 1.0; }
 	return Line(a, b, c);
 }
