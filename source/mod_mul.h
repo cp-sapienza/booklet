52bf1ac889db3e20caf135cf134791752a93ad48
Dario Petrillo <dario.pk1@gmail.com>
2023-01-07 14:43:36 +0100

    Prefer using __int128 in mod_mul

--- source/mod_mul.h
+++ source/mod_mul.h
@@ -2,6 +2,11 @@
 #include "common.h"
 
 ll mod_mul(ll a, ll b, ll mod) {
+    return (ll)((__int128)a * b % mod);
+}
+
+// SLOW version if __int128 isn't supported
+ll mod_mul(ll a, ll b, ll mod) {
 	ll res = 0;
 	for (a %= mod, b %= mod; b != 0; b >>= 1, a = (a << 1) % mod)
 		if (b & 1) res = (res + a) % mod;
