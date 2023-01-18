32c8089f44b249abdfb1ffddae0483528ae11f4a
Dario Petrillo <dario.pk1@gmail.com>
2023-01-18 21:25:26 +0100

    Fix initial state of index powers

--- source/linear_recurrences.h
+++ source/linear_recurrences.h
@@ -15,8 +15,12 @@ ll linear_recurrence(const vector<ll>& a, const vector<ll>& b, const vector<ll>&
 			m[k+i][k+j] = (m[k+i-1][k+j-1] + m[k+i-1][k+j]) % mod;
 	}
 
-	vector<ll> xx(k+h, 1);
+	vector<ll> xx(k+h);
 	for(int i = 0; i < k; i++) xx[i] = x[i];
+	if(h > 0) {
+        xx[k] = 1;
+	    for(int i = 1; i < h; i++) xx[k+i] = (xx[k+i-1] * k) % mod;
+    }
 
 	auto res = mat_mul(mat_pow(m, n, mod), xx, mod);
 	return (res[0] % mod + mod) % mod;
