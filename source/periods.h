af05ab6e7df599ff2cdc5671c254683a2d81c2ec
andrea <andreapudd@gmail.com>
2024-01-15 00:07:35 +0100

    style

--- source/periods.h
+++ source/periods.h
@@ -2,12 +2,10 @@
 #include "common.h"
 #include "z_func.h"
 
-vi periods(string s)
-{
+vi periods(string s) {
 	vi z = z_function(s), p;
 	int n = ssize(z);
-	for(int i = 1; i < n; i++)
-	{
+	for(int i = 1; i < n; i++) {
 		// if(n%i == 0) // uncomment for no trailing subperiods
 		if(z[i]+i == n)
 			p.push_back(i);
